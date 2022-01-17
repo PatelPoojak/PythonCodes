11fefe6c5bbab169d771047cd6970de3950f5003
PatelPoojak <91432946+PatelPoojak@users.noreply.github.com>
2022-01-16 23:50:23 -0600

    Update DataPrediction.py

--- DataPrediction.py
+++ DataPrediction.py
@@ -1,1 +1,51 @@
-print("yee
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn
+import numpy as np
+
+dataset = pd.read_csv('C:/Users/patel/Downloads/StudentGrades.csv')
+X = dataset.iloc[:, [1, 2, 3]].values
+y = dataset.iloc[:, -1].values
+
+z = X[:,1]
+
+print(z)
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:,1] = le.fit_transform(X[:,1])
+
+print(X)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+print(y_test)
+
+print(y_pred)
+
+from sklearn.metrics import confusion_matrix,accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+ac = accuracy_score(y_test,y_pred)
+
+print(cm)
+
+print(ac)
+
+X_new = [[0, 37, 80000]]
+
+Y_new = classifier.predict(X_new)
+
+print(Y_new)
