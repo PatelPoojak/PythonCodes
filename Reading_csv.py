aeadb971beac698d96d18ed44221154b70ee5dcd
PatelPoojak <91432946+PatelPoojak@users.noreply.github.com>
2022-03-26 10:57:46 -0500

    Create Reading_csv.py

---
+++ Reading_csv.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data = pd.read_csv('C:/xxxx/xxxxxxx/xxxxxxxxxx/xxxxxxxxxx.csv') #forward slashes --> get file location and put your own path in of your csv
+#cmds to test: data.head(), data.tail(), data.info()
